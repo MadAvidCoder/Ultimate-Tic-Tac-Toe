c441511a719ca44c7ce618a12914ea1653aeaa1e
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2024-11-27 16:18:17 +1100

    Set-up GitHub Pages Workflow
    Modified file structure, edited build templates and created a GitHub Actions build workflow.

--- click_check.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-signal clicked(path)
-
-func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
-	if event.is_action_pressed("click"):
-		clicked.emit(self.get_path())
